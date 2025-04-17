7544318c18a559157449319290b114707c2d292a
Ankit <contactankit1234@gmail.com>
2025-04-17 17:19:16 +0530

    add Hero component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Hero from './page/hero'
 function App() {
   return (
+    <>
+    <Hero />
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">
         Hello Tailwind CSS with Vite!
       </h1>
     </div>
+    </>
   )
 }
 
