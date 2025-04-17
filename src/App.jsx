132fae00442a287f2d4d75cf68e93778463ab471
sakshamshukla02 <sshuklaknp2002@gmail.com>
2025-04-17 17:30:56 +0530

    added nav to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import Navbar from "./page/navbar"
+
+
 function App() {
   return (
+    <>
+    <Navbar/>
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+
       <h1 className="text-4xl font-bold text-blue-600">
         Hello Tailwind CSS with Vite!
       </h1>
     </div>
+    </>
   )
 }
 
